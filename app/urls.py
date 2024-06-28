9f07eded49297afda420897436f6ba4ea6d61989
Aleejandra <mi.currihual@duocuc.cl>
2024-06-27 22:24:43 -0400

    arreglos

--- app/urls.py
+++ app/urls.py
@@ -5,12 +5,11 @@ from . import views
 urlpatterns = [
     path('', views.home, name='home'),
     path('productos/', views.productos, name='productos'),
-    path('contacto/', views.contacto, name='contacto'),
     path('servicios/', views.servicios, name='servicios'),
     path('banners/', views.banners, name='banners'),
     path('umbrella/', views.umbrella, name='umbrella'),
 
-    path('checkout/<int:id_suscripcion>/', views.CheckOut, name='checkout'),
+    # path('checkout/<int:id_suscripcion>/', views.CheckOut, name='checkout'),
     path('payment-success/<int:id_suscripcion>/', views.PaymentSuccessful, name='payment-success'),
     path('payment-failed/<int:id_suscripcion>/', views.PaymentFailed, name='payment-failed'),
 ]
--- app/views.py
+++ app/views.py
@@ -39,43 +39,12 @@ def productos(request):
 
     return render(request, 'app/productos.html', {"suscripciones_con_paypal": suscripciones_con_paypal})
 
-
-def contacto(request):
-    return render(request,'app/contacto.html')
-
 def servicios(request):
     return render(request, 'app/servicios.html')
 
 
-def CheckOut(request, id_suscripcion):
-    product = Suscripcion.objects.get(id=id_suscripcion)
-
-    host = request.get_host()
-
-    paypal_checkout = {
-        'business': settings.PAYPAL_RECEIVER_EMAIL,
-        'amount': product.precio_suscripcion,
-        'item_name': product.nombre_suscripcion,
-        'invoice': uuid.uuid4(),
-        'currency_code': 'USD',
-        'notify_url': f"http://{host}{reverse('paypal-ipn')}",
-        'return_url': f"http://{host}{reverse('payment-success', kwargs={'id_suscripcion': product.id})}",
-        'cancel_url': f"http://{host}{reverse('payment-failed', kwargs={'id_suscripcion': product.id})}",
-    }
-
-    paypal_payment = PayPalPaymentsForm(initial=paypal_checkout)
-
-    context = {
-        'product': product,
-        'paypal': paypal_payment,
-    }
-
-    return render(request, 'app/checkout.html', context)
-
-
 def PaymentSuccessful(request, id_suscripcion):
     product = Suscripcion.objects.get(id= id_suscripcion)
-
     return render(request, 'app/home.html', {'product': product})
 
 def PaymentFailed(request, id_suscripcion):
